2eb8a236380aed28b22144db896cfc5b085a0441
add-iv <frederick.2000@gmx.de>
2023-03-14 08:07:28 +0100

    change algorithm to strategy to match the pattern

--- pandaRec/recommender.py
+++ pandaRec/recommender.py
@@ -29,14 +29,15 @@ class Context:
         return selection.r1 == 0 and selection.r2 == self.data.shape[0]
 
 
-class SearchAlgorithm(ABC):
+class RecommendStrategy(ABC):
     @abstractmethod
     def search(self, context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
         pass
 
 
-class NameSearch(SearchAlgorithm):
-    def search(self, context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
+class NameSearch(RecommendStrategy):
+    @staticmethod
+    def search(context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
         result = []
         for recipe in recipes:
             if context.search in recipe.name:
@@ -49,26 +50,39 @@ class NameSearch(SearchAlgorithm):
 class Recommender:
     recipes: list[Recipe]
     context: Context
-    searchAlgorithm: SearchAlgorithm
+    recommendStrategy: RecommendStrategy
+    recommendedRecipes: list[RecipeResult]
 
     def __init__(
-        self, recipes: list[Recipe], context: Context, searchAlgorithm: SearchAlgorithm
+        self,
+        recipes: list[Recipe],
+        df: DataFrame,
+        searchAlgorithm: RecommendStrategy,
     ):
         self.recipes = recipes
-        self.context = context
+        self.context = Context([], df, "")
         self.searchAlgorithm = searchAlgorithm
+        self.recommendedRecipes = []
 
-    def recommend(self) -> list[RecipeResult]:
-        return self.searchAlgorithm.search(self.context, self.recipes)
+    def recommend(self):
+        self.recommendedRecipes = self.searchAlgorithm.search(
+            self.context, self.recipes
+        )
 
     def importRecipes(self, recipes: list[Recipe]):
         self.recipes = recipes
 
-    def showResults(self, recipeResults: list[RecipeResult]) -> str:
+    def showResults(self) -> str:
         result = ""
-        for recipeResult in recipeResults:
+        for recipeResult in self.recommendedRecipes:
             recipe = next(
                 (x for x in self.recipes if x.id == recipeResult.recipeId), None
             )
             result += f"{recipe}: {recipeResult.score}\n"
         return result
+
+    def set_Search(self, search: str):
+        self.context.search = search
+
+    def set_Selection(self, selections: list[Selection]):
+        self.context.selections = selections
