834cdced4fea327f28cf6d2b008b3bd5c7acef45
add-iv <frederick.2000@gmx.de>
2023-03-14 08:05:59 +0100

    put the ui in a widget

---
+++ pandaRec/pandaRecWidget.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import ipywidgets as widgets
+import ipydatagrid
+from .recommender import Recommender
+
+
+class PandaRecWidget(widgets.VBox):
+    def __init__(self, recommender: Recommender) -> None:
+        super().__init__()
+
+        self.recommender = recommender
+
+        self.data_grid = ipydatagrid.DataGrid(
+            self.recommender.context.data,
+            editable=True,
+            layout=widgets.Layout(height="100px"),
+        )
+
+        self.search_term_widget = widgets.Text(
+            placeholder="Search term", description="Search:"
+        )
+
+        self.search_term_widget.observe(self.update_recommendations, names='value')
+        self.data_grid.observe(self.update_recommendations, names="selected_cells")
+
+        self.children = [self.data_grid, self.search_term_widget]
+
+    def update_recommendations(self, change):
+        self.recommender.set_Search(str(self.search_term_widget.value))
+        self.recommender.set_Selection(self.data_grid.selected_cells)
+        self.recommender.recommend()
+        print(self.recommender.showResults())
