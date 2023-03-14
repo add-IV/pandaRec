05e934544499d48ea648db8a1dfcb11ed7b499e3
add-iv <frederick.2000@gmx.de>
2023-03-14 03:50:23 +0100

    add from dict for yaml import

--- pandaRec/recipe.py
+++ pandaRec/recipe.py
@@ -1,23 +1,35 @@
 from dataclasses import dataclass
 
-class Recipe():
+
+class Recipe:
     id: int
     name: str
     description: str
     code: str
     keywords: str
 
-    def __init__(self, id, name, description, code, keywords):
+    def __init__(self, id: int, name: str, description: str, code: str, keywords: str):
         self.id = id
         self.name = name
         self.description = description
         self.code = code
         self.keywords = keywords
-    
-    def __str__(self):
+
+    def __str__(self) -> str:
         return f"Recipe({self.id}, {self.name}, {self.description}, {self.code}, {self.keywords})"
 
+    @staticmethod
+    def from_dict(d: dict):
+        return Recipe(
+            d["id"],
+            d["name"],
+            d["description"],
+            d["code"],
+            d["keywords"],
+        )
+
+
 @dataclass
-class RecipeResult():
+class RecipeResult:
     score: float
     recipeId: int
