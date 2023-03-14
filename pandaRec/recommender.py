625ff5436e4762e9c2ac1022f2ae3fec6c2f02f5
add-iv <frederick.2000@gmx.de>
2023-03-14 03:50:39 +0100

    replace datagrid with dataframe

--- pandaRec/recommender.py
+++ pandaRec/recommender.py
@@ -1,19 +1,27 @@
-from ipydatagrid import DataGrid
 from abc import ABC, abstractmethod
 from .recipe import Recipe, RecipeResult
 from dataclasses import dataclass
+from pandas import DataFrame
+
 
 @dataclass
-class Selection():
+class Selection:
     r1: int
     c1: int
     r2: int
     c2: int
 
 
-class context():
+class context:
     selections: list[Selection]
+    data: DataFrame
     search: str
+
+    def isWholeRow(self, selection: Selection) -> bool:
+        return selection.c1 == 0 and selection.c2 == self.data.shape[1]
+
+    def isWholeColumn(self, selection: Selection) -> bool:
+        return selection.r1 == 0 and selection.r2 == self.data.shape[0]
 
 
 class SearchAlgorithm(ABC):
@@ -21,9 +29,8 @@ class SearchAlgorithm(ABC):
     def search(self, context: context, recipes: list[Recipe]) -> list[RecipeResult]:
         pass
 
-class Recommender():
+
+class Recommender:
     recipes: list[Recipe]
     context: context
-    grid: DataGrid
     searchAlgorithm: SearchAlgorithm
-
