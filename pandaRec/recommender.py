ef27d21178bedda4bb101717261972e338fe5684
add-iv <frederick.2000@gmx.de>
2023-03-14 04:44:36 +0100

    add search functionality

--- pandaRec/recommender.py
+++ pandaRec/recommender.py
@@ -12,10 +12,15 @@ class Selection:
     c2: int
 
 
-class context:
+class Context:
     selections: list[Selection]
     data: DataFrame
     search: str
+
+    def __init__(self, selections: list[Selection], data: DataFrame, search: str):
+        self.selections = selections
+        self.data = data
+        self.search = search
 
     def isWholeRow(self, selection: Selection) -> bool:
         return selection.c1 == 0 and selection.c2 == self.data.shape[1]
@@ -26,11 +31,44 @@ class context:
 
 class SearchAlgorithm(ABC):
     @abstractmethod
-    def search(self, context: context, recipes: list[Recipe]) -> list[RecipeResult]:
+    def search(self, context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
         pass
+
+
+class NameSearch(SearchAlgorithm):
+    def search(self, context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
+        result = []
+        for recipe in recipes:
+            if context.search in recipe.name:
+                result.append(RecipeResult(1, recipe.id))
+            else:
+                result.append(RecipeResult(0, recipe.id))
+        return result
 
 
 class Recommender:
     recipes: list[Recipe]
-    context: context
+    context: Context
     searchAlgorithm: SearchAlgorithm
+
+    def __init__(
+        self, recipes: list[Recipe], context: Context, searchAlgorithm: SearchAlgorithm
+    ):
+        self.recipes = recipes
+        self.context = context
+        self.searchAlgorithm = searchAlgorithm
+
+    def recommend(self) -> list[RecipeResult]:
+        return self.searchAlgorithm.search(self.context, self.recipes)
+
+    def importRecipes(self, recipes: list[Recipe]):
+        self.recipes = recipes
+
+    def showResults(self, recipeResults: list[RecipeResult]) -> str:
+        result = ""
+        for recipeResult in recipeResults:
+            recipe = next(
+                (x for x in self.recipes if x.id == recipeResult.recipeId), None
+            )
+            result += f"{recipe}: {recipeResult.score}\n"
+        return result
