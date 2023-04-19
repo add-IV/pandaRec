f34a3b023a71f6c60722d6b52eadd70743eeb919
add-iv <frederick.2000@gmx.de>
2023-04-19 16:14:05 +0200

    run black

--- pandaRec/strategies.py
+++ pandaRec/strategies.py
@@ -18,21 +18,28 @@ class NameSearch(RecommendStrategy):
             if context.search in recipe.name:
                 result.append(RecipeResult(1, recipe.id))
         return result
-    
+
+
 class FuzzySearchTitle(RecommendStrategy):
     @staticmethod
     def search(context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
         titles = [recipe.name for recipe in recipes]
-        extracted: list[tuple[str, int]] = process.extract(context.search, titles, limit=20) # type: ignore
-        result = [RecipeResult(one[1], getResultId(one[0], recipes)) for one in extracted]
+        extracted: list[tuple[str, int]] = process.extract(context.search, titles, limit=20)  # type: ignore
+        result = [
+            RecipeResult(one[1], getResultId(one[0], recipes)) for one in extracted
+        ]
         return result
-    
+
+
 class FuzzySearchDescription(RecommendStrategy):
     def __init__(self, ratio=fuzz.partial_ratio):
         super().__init__()
         self.ratio = ratio
 
     def search(self, context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
-        result = [RecipeResult(self.ratio(context.search, recipe.description), recipe.id) for recipe in recipes]
-        result.sort(key = lambda recipeResult:recipeResult.score, reverse=True)
+        result = [
+            RecipeResult(self.ratio(context.search, recipe.description), recipe.id)
+            for recipe in recipes
+        ]
+        result.sort(key=lambda recipeResult: recipeResult.score, reverse=True)
         return result
