42059949c75206b4d99982aeaeab7cd1a0968792
add-iv <frederick.2000@gmx.de>
2023-04-19 15:36:11 +0200

    backwards compatipility

--- pandaRec/recipe.py
+++ pandaRec/recipe.py
@@ -40,5 +40,5 @@ class RecipeResult:
 def getResultId(name: str, recipes: list[Recipe]) -> int:
     return next((recipe.id for recipe in recipes if name == recipe.name), -1)
 
-def getRecipeById(id: int, recipes: list[Recipe]) -> Recipe | None:
+def getRecipeById(id: int, recipes: list[Recipe]) -> "Recipe | None":
     return next((recipe for recipe in recipes if id == recipe.id), None)
