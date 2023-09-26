ee49ab3f7f224d9692d876056b75f7f2a534cddf
add-iv <frederick.2000@gmx.de>
2023-09-26 22:15:54 +0200

    add new files to init.py

--- pandaRec/__init__.py
+++ pandaRec/__init__.py
@@ -2,3 +2,5 @@ from pandaRec.pandaRecWidget import PandaRecWidget
 from pandaRec.recipe import Recipe
 from pandaRec.recommender import Recommender
 from pandaRec import strategies
+from pandaRec import lemmatize
+from pandaRec import ml_embeddings
