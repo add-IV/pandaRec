c3c72c678e8a2d23e986c185bb0af5e79e0f2f1f
add-iv <frederick.2000@gmx.de>
2023-09-26 22:16:07 +0200

    add index search

--- pandaRec/strategies.py
+++ pandaRec/strategies.py
@@ -3,7 +3,12 @@ from .context import Context
 from .recipe import *
 from thefuzz import fuzz, process
 from sentence_transformers import SentenceTransformer, util
-from torch import save, load
+from .ml_embeddings import generate_embeddings_sentence_transformers, load_embeddings
+from .lemmatize import (
+    generate_search_index,
+    load_search_index,
+    get_lemmatized_no_stop_words,
+)
 
 
 class RecommendStrategy(ABC):
@@ -42,32 +47,58 @@ class FuzzySearchDescription(RecommendStrategy):
         self.ratio = ratio
 
     def search(self, context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
+        scores = len(recipes) * [0]
+        words = context.search.split()
+        for word in words:
+            for idx in range(len(recipes)):
+                scores[idx] += self.ratio(word, recipes[idx].description)
+        scores = [score / len(words) for score in scores]
+        result = [RecipeResult(score, recipe) for score, recipe in zip(scores, recipes)]
+        result.sort(key=lambda recipeResult: recipeResult.score, reverse=True)
+        return result
+
+
+class IndexSearch(RecommendStrategy):
+    def __init__(self, recipes: list[Recipe], path: str = "", ratio=fuzz.partial_ratio):
+        super().__init__()
+        if not path:
+            self.index = generate_search_index(
+                [recipe.description for recipe in recipes]
+            )
+        else:
+            self.index = load_search_index(path)
+
+    def search(self, context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
+        lemmatized_search = get_lemmatized_no_stop_words(context.search)
+        scores = len(recipes) * [0]
+        num_words = len(lemmatized_search)
+        for word in lemmatized_search:
+            if word in self.index:
+                for idx, _ in self.index[word]:
+                    scores[idx] += 1
         result = [
-            RecipeResult(self.ratio(context.search, recipe.description), recipe)
-            for recipe in recipes
+            RecipeResult(score / num_words, recipe)
+            for score, recipe in zip(scores, recipes)
         ]
         result.sort(key=lambda recipeResult: recipeResult.score, reverse=True)
         return result
 
+
 class SemanticSearch(RecommendStrategy):
-    def __init__(self, recipes: list[Recipe], path: str = None):
+    def __init__(self, recipes: list[Recipe], path: str = ""):
         super().__init__()
         self.model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
         descriptions = [recipe.description for recipe in recipes]
         if not path:
-            self.embeddings = self.model.encode(descriptions, convert_to_tensor=True)
+            self.embeddings = generate_embeddings_sentence_transformers(descriptions)
         else:
-            self.load_embeddings(path)
+            self.embeddings = load_embeddings(path)
 
     def search(self, context: Context, recipes: list[Recipe]) -> list[RecipeResult]:
         query_embedding = self.model.encode(context.search, convert_to_tensor=True)
-        cos_scores = [util.cos_sim(query_embedding, embedding).item() for embedding in self.embeddings]
-        result = [RecipeResult(score, recipe) for recipe, score in zip(recipes, cos_scores)]
+        cos_scores = [util.cos_sim(query_embedding, embedding).item() for embedding in self.embeddings]  # type: ignore
+        result = [
+            RecipeResult(score, recipe) for recipe, score in zip(recipes, cos_scores)
+        ]
         result.sort(key=lambda recipeResult: recipeResult.score, reverse=True)
         return result
-
-    def save_embeddings(self, path):
-        save(self.embeddings, path)
-
-    def load_embeddings(self, path):
-        self.embeddings = load(path)
