7ecd07837604aa3d82f9076902d6a1f9c7eb6735
add-iv <frederick.2000@gmx.de>
2023-09-29 04:19:19 +0200

    rename file

--- pandaRec/__init__.py
+++ pandaRec/__init__.py
@@ -2,5 +2,5 @@ from pandaRec.panda_rec_widget import PandaRecWidget
 from pandaRec.recipe import Recipe
 from pandaRec.recommender import Recommender
 from pandaRec import strategies
-from pandaRec import lemmatize
+from pandaRec import search_index
 from pandaRec import ml_embeddings
