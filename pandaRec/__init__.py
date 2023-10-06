c648bdaf1b48dfcab78301ad584abc84cdbf6c79
add-iv <frederick.2000@gmx.de>
2023-10-06 05:32:43 +0200

    add recipescollection to init

--- pandaRec/__init__.py
+++ pandaRec/__init__.py
@@ -1,5 +1,5 @@
 from pandaRec.panda_rec_widget import PandaRecWidget
-from pandaRec.recipe import Recipe
+from pandaRec.recipe import Recipe, RecipeCollection
 from pandaRec.recommender import Recommender
 from pandaRec import strategies
 from pandaRec import search_index
